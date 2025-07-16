849a7b890bdd55ac38fc2f63c713700a68e47ee7
petermyo <p3t3rmyo@gmail.com>
2025-07-17 00:45:59 +0630

    Update _middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -38,12 +38,10 @@ export const onRequest = async (context) => {
         return handleApiRequest(request, env);
     }
     if (path.startsWith('/s/')) {
-        if (request.method === 'GET') return handleShortUrlGet(request, env);
-        if (request.method === 'POST') return handleShortUrlPost(request, env);
+        return handleShortUrl(request, env);
     }
     if (path.startsWith('/l/')) {
-        if (request.method === 'GET') return handlePublicListGet(request, env);
-        if (request.method === 'POST') return handlePublicListPost(request, env);
+        return handlePublicList(request, env);
     }
     
     // For all other requests (e.g., /, /admin), let Pages serve the static HTML file.
