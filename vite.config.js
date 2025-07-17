89b17d01f01afbed348fb2bdf14311ce3478037b
petermyo <p3t3rmyo@gmail.com>
2025-07-17 16:03:57 +0630

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,18 +5,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // FIX: Explicitly set publicDir to false to avoid the build warning.
-  // This tells Vite that we are not using a separate public assets folder.
-  publicDir: false,
+  publicDir: true,
   build: {
-    outDir: 'public',
+    outDir: 'public'
   },
   server: {
     proxy: {
       '/api': 'http://localhost:8788',
       '/s': 'http://localhost:8788',
-      '/l': 'http://localhost:8788',
-      '/': 'http://localhost:8788',
+      '/l': 'http://localhost:8788'
     },
   },
 })
