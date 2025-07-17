6d79415a9aa923839b8996e6ec28e6d7afc013d6
petermyo <p3t3rmyo@gmail.com>
2025-07-17 09:53:31 +0630

    Create AdminDashboard.jsx

---
+++ src/pages/admin/AdminDashboard.jsx
@@ -0,0 +1,24 @@
+// --- /src/pages/admin/AdminDashboard.jsx ---
+import React from 'react';
+const AdminDashboard = () => <div>Admin Dashboard Content</div>;
+export default AdminDashboard;
+
+// --- /src/pages/admin/AdminUsers.jsx ---
+import React from 'react';
+const AdminUsers = () => <div>Admin Users Content</div>;
+export default AdminUsers;
+
+// --- /src/pages/admin/AdminUserDetail.jsx ---
+import React from 'react';
+const AdminUserDetail = () => <div>Admin User Detail Content</div>;
+export default AdminUserDetail;
+
+// --- /src/pages/admin/AdminFiles.jsx ---
+import React from 'react';
+const AdminFiles = () => <div>Admin Files Content</div>;
+export default AdminFiles;
+
+// --- /src/pages/admin/AdminLists.jsx ---
+import React from 'react';
+const AdminLists = () => <div>Admin Lists Content</div>;
+export default AdminLists;
