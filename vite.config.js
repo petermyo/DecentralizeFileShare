df5f84a4a07bec3fdd035254e24e19271eaafcdd
petermyo <p3t3rmyo@gmail.com>
2025-07-17 12:23:22 +0630

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
   build: {
-    // This line tells Vite to create a 'public' folder for the build output,
-    // matching your Cloudflare Pages project settings.
     outDir: 'public',
   },
   server: {
-    // This proxy is for local development and does not affect the build.
     proxy: {
       '/api': 'http://localhost:8788',
       '/s': 'http://localhost:8788',
